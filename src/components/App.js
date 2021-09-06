68a42abe65202c551c386a1c8c4056eaf5c06f50
VikaLevchyshyna <vika.levchyshyna@gmail.com>
2021-09-06 13:56:18 +0300

    fix import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
